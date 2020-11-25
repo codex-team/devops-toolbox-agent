a48fd2671eaaf9725d0140f212730ae35ad0ba36
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-25 20:27:04 +0300

    Update message.ts

--- src/utils/protocol/types/message.ts
+++ src/utils/protocol/types/message.ts
@@ -1,7 +1,7 @@
 /**
  * Message can contain any data at payload
  */
-export type MessagePayload = { [key: string]: any }
+export type MessagePayload = { [key: string]: any };
 
 /**
  * Any client-server message should fit this structure
