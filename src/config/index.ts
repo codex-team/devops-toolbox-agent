c083c4b9353748c383eef37732e0fb4a5f1602e6
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-25 13:32:01 +0300

    Create class Config

---
+++ src/config/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+export default class Config {
+  /**
+   * Api url
+   */
+  public static apiUrl: string = process.env.API_URL!;
+  /**
+   * Cron schedule
+   */
+  public static schedule: string = process.env.SCHEDULE!;
+  /**
+   * Server token
+   */
+  public static token: string = process.env.Token!;
+}
