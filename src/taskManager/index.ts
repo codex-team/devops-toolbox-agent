bfc082f0429681d632a383dba41e59ba1cec9e15
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-26 14:30:43 +0300

    Rename class and updare JSDoc

--- src/taskManager/index.ts
+++ src/taskManager/index.ts
@@ -1,11 +1,12 @@
 import cron from 'node-cron';
 
 /**
- * Class Cron
+ * Class TaskManager. TaskManager does the task on a schedule.
+ * You can set schedule and task.
  */
-export default class Cron {
+export default class TaskManager {
   /**
-   * @private scheduledTask - cron task
+   * @private scheduledTask - scheduled task
    */
   private scheduledTask: cron.ScheduledTask;
 
@@ -13,7 +14,7 @@ export default class Cron {
    * Constructor
    *
    * @param schedule - schedule
-   * @param task - cron task
+   * @param task - task
    */
   constructor(schedule: string, task: () => Promise<void>) {
     this.scheduledTask = cron.schedule(schedule, task);
