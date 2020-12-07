312d784d7496eb0e311312d7725a81194f0dfc7c
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-12-07 21:47:50 +0300

    Created class parser for nginx config

---
+++ src/utils/parsers/nginx/index.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import { NginxPayload } from './types';
+const ConfigParser = require('@webantic/nginx-config-parser');
+
+/**
+ * Nginx Parser
+ */
+export default class NginxParser {
+  /**
+   * The method that parses all nginx configs
+   *
+   * @param path - absolute path to nginx/sites-enabled
+   */
+  public static parse(path: string): NginxPayload[] {
+    const files = fs.readdirSync(path).filter(file => file.match(/\.conf/));
+
+    const parser = new ConfigParser();
+    const configs = files.map(file => parser.readConfigFile(path + '/' + file, {
+      parseIncludes: false,
+    }));
+
+    const nginxPayloads: NginxPayload[] = [];
+
+    for (const config of configs) {
+      const httpServer = config.http?.server;
+
+      if (!httpServer) {
+        continue;
+      }
+
+      // eslint-disable-next-line camelcase
+      const { listen, server_name: serverName, 'location /': location } = httpServer;
+
+      const payload: NginxPayload = {
+        listen,
+        serverName,
+        proxyPass: location.proxy_pass,
+      };
+
+      nginxPayloads.push(payload);
+    }
+
+    return nginxPayloads;
+  }
+}
